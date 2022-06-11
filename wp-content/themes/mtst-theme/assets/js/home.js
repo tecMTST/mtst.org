cebb43e4d5ddda83a1e0c999e0e324117e08fd77
dsilvadori <dev.maquinads@gmail.com>
2022-06-11 12:11:41 -0300

    Cria a seção de Fotos da Home

---
+++ wp-content/themes/mtst-theme/assets/js/home.js
@@ -0,0 +1,26 @@
+const swiper = new Swiper('.swiper', {
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+  speed: 500,
+  autoplay: {
+    delay: 4000,
+  },
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },    
+  }
+})
+
+Swiper()
---
+++ wp-content/themes/mtst-theme/template-parts/home/fotos.php
@@ -0,0 +1,46 @@
+            <section class="fotos" id="fotos">
+                <div class="container">
+                    <h2 class="titulo-secao">FOTOS</h2>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="foto foto-maior">
+                                <div class="swiper swiper-foto">
+                                    <div class="swiper-wrapper">
+                                        <div class="swiper-slide">
+                                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/insta01.jpg" alt="Foto do Instagram">
+                                        </div>
+                                        <div class="swiper-slide">
+                                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/insta02.jpg" alt="Foto do Instagram">
+                                        </div>
+                                        <div class="swiper-slide">
+                                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/insta03.jpg" alt="Foto do Instagram">
+                                        </div>
+                                    </div>
+                                    <div class="swiper-pagination"></div>
+                                    <div class="swiper-button-prev"></div>
+                                    <div class="swiper-button-next"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="foto foto-menor">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/insta04.jpg" alt="Foto do Instagram">
+                            </div>
+                            <div class="foto foto-menor">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/insta06.jpg" alt="Foto do Instagram">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="foto foto-menor">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/insta05.jpg" alt="Foto do Instagram">
+                            </div>
+                            <div class="foto foto-menor">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/insta07.jpg" alt="Foto do Instagram">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="ver-tudo">
+                        <a target="_blank" href="https://www.instagram.com/mtstbrasil/">VER TUDO >>></a>
+                    </div>
+                </div>
+            </section>
