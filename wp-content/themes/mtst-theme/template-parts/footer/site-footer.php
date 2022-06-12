34d92d939039cc84df8f82914020b0dcddc31ff3
dsilvadori <dev.maquinads@gmail.com>
2022-06-11 22:46:48 -0300

    Implementa o layout do Rodapé do site

---
+++ wp-content/themes/mtst-theme/template-parts/footer/site-footer.php
@@ -0,0 +1,15 @@
+            <div class="site-footer">
+                <div class="container">
+                    <div class="txt-footer">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis tellus vulputate sit lectus. Non consectetur a sapien, ut interdum ridiculus sagittis. Nisl blandit morbi est quis ac dolor sed. Amet quisque ut faucibus quis vel integer ullamcorper.</p>
+                    </div>
+                    <nav class="nav-footer">
+                        <ul>
+                            <li><a href="#">Contato</a></li>
+                            <li><a href="#">Packs</a></li>
+                            <li><a href="#">Assessoria</a></li>
+                            <li><a href="#">Cartase</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
--- wp-content/themes/mtst-theme/template-parts/home/videos.php
+++ wp-content/themes/mtst-theme/template-parts/home/videos.php
@@ -24,14 +24,14 @@
                             </a>
                         </div>
                         <div class="youtube-video ltt">
-                            <a href="https://youtu.be/eZFNw9gLkDY" class="vp-a">
-                                <img src="https://img.youtube.com/vi/eZFNw9gLkDY/mqdefault.jpg" alt="Vídeo">
+                            <a href="https://youtu.be/VUBkivHBczw" class="vp-a">
+                                <img src="https://img.youtube.com/vi/VUBkivHBczw/mqdefault.jpg" alt="Vídeo">
                                 <img class="player-video" src="<?php echo get_template_directory_uri(); ?>/assets/images/player.png" alt="">
                             </a>
                         </div>
                         <div class="youtube-video ltt">
-                            <a href="https://youtu.be/eZFNw9gLkDY" class="vp-a">
-                                <img src="https://img.youtube.com/vi/eZFNw9gLkDY/mqdefault.jpg" alt="Vídeo">
+                            <a href="https://youtu.be/9eS6Q4ll4fY" class="vp-a">
+                                <img src="https://img.youtube.com/vi/9eS6Q4ll4fY/mqdefault.jpg" alt="Vídeo">
                                 <img class="player-video" src="<?php echo get_template_directory_uri(); ?>/assets/images/player.png" alt="">
                             </a>
                         </div>
@@ -39,26 +39,26 @@
                     <div class="col-md-3">
                         <div class="txt-video-r">
                             <div class="titulo-video ltt">
-                                <h3>Ediane Maria - Conheça essa guerreira</h3>
+                                <h3>Jogo do Ano! Apoie as cozinhas solidárias</h3>
                             </div>  
                             <div class="desc ltt">
-                                <p>Quem é essa tal de Ediane Maria, pré candidata a deputada estadual de SP? Vem descobrir aqui com a gente! A mudança será de dentro pra fora, será coletiva, periférica, esse novo desafio é nosso! Bora?!</p>
+                                <p>Show de solidariedade! Já de futebol... Com Craque Neto, Dexter, MC Max B.O. e Ediane. Participe também no <a target="_blank" href="https://apoia.se/cozinhasolidaria">apoia.se/cozinhasolidaria.</a></p>
                             </div>
                         </div>
                         <div class="txt-video-r">
                             <div class="titulo-video ltt">
-                                <h3>Ediane Maria - Conheça essa guerreira</h3>
+                                <h3>Cultura - Ciranda Luciana Ferreira</h3>
                             </div>  
                             <div class="desc ltt">
-                                <p>Quem é essa tal de Ediane Maria, pré candidata a deputada estadual de SP? Vem descobrir aqui com a gente! A mudança será de dentro pra fora, será coletiva, periférica, esse novo desafio é nosso! Bora?!</p>
+                                <p>Nesta nossa Quarta Cultural, vamos ver alguns registros da construção da Ciranda de Arte e Cultura  Luciana Ferreira , na ocupação Luisa Mahin em homenagem à vida e ao legado de nossa grande companheira!</p>
                             </div>
                         </div>
                         <div class="txt-video-r">
                             <div class="titulo-video ltt">
-                                <h3>Ediane Maria - Conheça essa guerreira</h3>
+                                <h3>Famílias do MTST receberam chaves de 910 apartamentos em Santo André</h3>
                             </div>  
                             <div class="desc ltt">
-                                <p>Quem é essa tal de Ediane Maria, pré candidata a deputada estadual de SP? Vem descobrir aqui com a gente! A mudança será de dentro pra fora, será coletiva, periférica, esse novo desafio é nosso! Bora?!</p>
+                                <p>Após mais de 7 anos de luta, fruto da resistência dentro das ocupações do MTST, os condomínios Novo Pinheirinho e Santo Dias, foram inaugurados em março de 2019.</p>
                             </div>
                         </div>
                     </div>
