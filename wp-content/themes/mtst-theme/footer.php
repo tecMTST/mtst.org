79c62cf44e21f21ba705536cd04d8f2d82798522
dsilvadori <dev.maquinads@gmail.com>
2022-06-12 20:37:47 -0300

    Configura os breadcrumbs

--- wp-content/themes/mtst-theme/footer.php
+++ wp-content/themes/mtst-theme/footer.php
@@ -4,6 +4,8 @@
 		<?php get_template_part( 'template-parts/footer/newsletter' ); ?>
 		<?php get_template_part( 'template-parts/footer/site-footer' ); ?>
 	</footer>
+	<?php if (slug()=='home') : ?>
 	<script type='text/javascript' src='<?php echo get_template_directory_uri(); ?>/assets/js/<?php echo $post->post_name; ?>.js'></script>
+	<?php endif; ?>
 	</body>
 </html>
--- wp-content/themes/mtst-theme/page-o-mtst.php
+++ wp-content/themes/mtst-theme/page-o-mtst.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
     <main>
         <?php get_template_part( 'template-parts/o-mtst/banner-topo' ); ?>
+        <?php get_template_part( 'template-parts/breadcrumbs/breadcrumbs' ); ?>
     </main>
 <?php get_footer(); ?>
