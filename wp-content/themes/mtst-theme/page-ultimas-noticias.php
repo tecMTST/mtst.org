67d8e0218d362b2886a59d7b765881cd0af79d46
dsilvadori <dev.maquinads@gmail.com>
2022-06-13 17:39:44 -0300

    Implementa o topo e o breadcrumb em notícias

---
+++ wp-content/themes/mtst-theme/page-ultimas-noticias.php
@@ -0,0 +1,6 @@
+<?php get_header(); ?>
+    <main>
+        <?php get_template_part( 'template-parts/noticias/topo' ); ?>
+        <?php get_template_part( 'template-parts/breadcrumbs/breadcrumbs' ); ?>
+    </main>
+<?php get_footer(); ?>
