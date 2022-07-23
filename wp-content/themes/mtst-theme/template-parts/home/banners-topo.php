da390d44057f72029e9ee987bdd734febe7df805
dsilvadori <dev.maquinads@gmail.com>
2022-07-23 20:43:34 -0300

    Ajustes mobile

--- wp-content/themes/mtst-theme/template-parts/home/banners-topo.php
+++ wp-content/themes/mtst-theme/template-parts/home/banners-topo.php
@@ -1,22 +1,24 @@
         <section class="banners">
-            <div class="container row">
-                <div class="banner-topo col-12">
-                    <a href="https://apoia.se/cozinhasolidaria" target="_blank" rel="noopener noreferrer">
-                        <img class="desktop-view" src="<?php echo get_template_directory_uri(); ?>/assets/images/banner-cozinhas-solidarias.jpg" alt="Apoia-se Cozinhas Solidárias">
-                        <img class="mobile-view" src="<?php echo get_template_directory_uri(); ?>/assets/images/banner-cozinhas-mobile.jpg" alt="Apoia-se Cozinhas Solidárias">
-                    </a>
-                </div>
-                <div class="banner-topo col-md-12 col-4">
-                    <a href="<?php echo home_url(); ?>/quem-somos/fake-news" target="_blank" rel="noopener noreferrer">
-                        <img class="desktop-view" src="<?php echo get_template_directory_uri(); ?>/assets/images/banner-fake-news.jpg" alt="Não caia em Fake News">
-                        <img class="mobile-view" src="<?php echo get_template_directory_uri(); ?>/assets/images/banner-fakenews-mobile.jpg" alt="Não caia em Fake News">
-                    </a>
-                </div>
-                <div class="banner-topo col-md-12 col-8">
-                    <a href="https://contratequemluta.com/" target="_blank" rel="noopener noreferrer">
-                        <img class="desktop-view" src="<?php echo get_template_directory_uri(); ?>/assets/images/banner-contrate.jpg" alt="Não caia em Fake News">
-                        <img class="mobile-view" src="<?php echo get_template_directory_uri(); ?>/assets/images/banner-cql-mobile.jpg" alt="Não caia em Fake News">
-                    </a>
+            <div class="container">
+                <div class="row">
+                    <div class="banner-topo col-12">
+                        <a href="https://apoia.se/cozinhasolidaria" target="_blank" rel="noopener noreferrer">
+                            <img class="desktop-view" src="<?php echo get_template_directory_uri(); ?>/assets/images/banner-cozinhas-solidarias.jpg" alt="Apoia-se Cozinhas Solidárias">
+                            <img class="mobile-view" src="<?php echo get_template_directory_uri(); ?>/assets/images/banner-cozinhas-mobile.jpg" alt="Apoia-se Cozinhas Solidárias">
+                        </a>
+                    </div>
+                    <div class="banner-topo col-md-12 col-4">
+                        <a href="<?php echo home_url(); ?>/quem-somos/fake-news" target="_blank" rel="noopener noreferrer">
+                            <img class="desktop-view" src="<?php echo get_template_directory_uri(); ?>/assets/images/banner-fake-news.jpg" alt="Não caia em Fake News">
+                            <img class="mobile-view" src="<?php echo get_template_directory_uri(); ?>/assets/images/banner-fakenews-mobile.jpg" alt="Não caia em Fake News">
+                        </a>
+                    </div>
+                    <div class="banner-topo col-md-12 col-8">
+                        <a href="https://contratequemluta.com/" target="_blank" rel="noopener noreferrer">
+                            <img class="desktop-view" src="<?php echo get_template_directory_uri(); ?>/assets/images/banner-contrate.jpg" alt="Não caia em Fake News">
+                            <img class="mobile-view" src="<?php echo get_template_directory_uri(); ?>/assets/images/banner-cql-mobile.jpg" alt="Não caia em Fake News">
+                        </a>
+                    </div>
                 </div>
             </div>
         </section>
