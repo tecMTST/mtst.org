bb7c4720e23e1e9aa721e58936ee3cb23856e19f
dsilvadori <dev.maquinads@gmail.com>
2022-07-24 03:30:55 -0300

    Refatoração da área de vídeos

--- wp-content/themes/mtst-theme/template-parts/home/videos.php
+++ wp-content/themes/mtst-theme/template-parts/home/videos.php
@@ -3,12 +3,12 @@
                 <h2 class="titulo-secao">VÍDEOS</h2>
                 <div class="row">
                     <div class="col-md-6">
-                        <div class="youtube-video">
-                            <a href="https://youtu.be/FOB9YL-6ZIo" class="vp-a">
-                                <img src="https://img.youtube.com/vi/FOB9YL-6ZIo/maxresdefault.jpg" alt="Vídeo">
+                        <a href="https://youtu.be/FOB9YL-6ZIo" class="vp-a">
+                            <div class="youtube-video" style="background:url(https://img.youtube.com/vi/FOB9YL-6ZIo/maxresdefault.jpg)">
+                                <!-- <img src="https://img.youtube.com/vi/FOB9YL-6ZIo/maxresdefault.jpg" alt="Vídeo"> -->
                                 <img class="player-video-big" src="<?php echo get_template_directory_uri(); ?>/assets/images/player-b.png" alt="">
-                            </a>
-                        </div>
+                            </div>
+                        </a>
                         <div class="titulo-video">
                             <h3>Ediane Maria - Conheça essa guerreira</h3>
                         </div>
@@ -16,7 +16,7 @@
                             <p>Quem é essa tal de Ediane Maria, pré candidata a deputada estadual de SP? Vem descobrir aqui com a gente! A mudança será de dentro pra fora, será coletiva, periférica, esse novo desafio é nosso! Bora?!</p>
                         </div>
                     </div>
-                    <div class="col-md-3">
+                    <!-- <div class="col-md-3">
                         <div class="youtube-video ltt">
                             <a href="https://youtu.be/eZFNw9gLkDY" class="vp-a">
                                 <img src="https://img.youtube.com/vi/eZFNw9gLkDY/mqdefault.jpg" alt="Vídeo">
@@ -61,6 +61,65 @@
                                 <p>Após mais de 7 anos de luta, fruto da resistência dentro das ocupações do MTST, os condomínios Novo Pinheirinho e Santo Dias, foram inaugurados em março de 2019.</p>
                             </div>
                         </div>
+                    </div> -->
+                    <div class="col-md-6">
+                        <ul class="videos">
+                            <li> 
+                                <a href="https://youtu.be/eZFNw9gLkDY" class="vp-a">
+                                    <div class="youtube-video ltt" style="background-image:url(https://img.youtube.com/vi/eZFNw9gLkDY/mqdefault.jpg)">
+                                        <img class="player-video" src="<?php echo get_template_directory_uri(); ?>/assets/images/player.png" alt="">
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <div class="txt-video-r">
+                                    <div class="titulo-video ltt">
+                                        <h3>Jogo do Ano! Apoie as cozinhas solidárias</h3>
+                                    </div>  
+                                    <div class="desc ltt">
+                                        <p>Show de solidariedade! Já de futebol... Com Craque Neto, Dexter, MC Max B.O. e Ediane. Participe também no <a target="_blank" href="https://apoia.se/cozinhasolidaria">apoia.se/cozinhasolidaria.</a></p>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                        <ul class="videos">
+                            <li> 
+                                <a href="https://youtu.be/VUBkivHBczw" class="vp-a">
+                                    <div class="youtube-video ltt" style="background-image:url(https://img.youtube.com/vi/VUBkivHBczw/mqdefault.jpg)">
+                                        <img class="player-video" src="<?php echo get_template_directory_uri(); ?>/assets/images/player.png" alt="">
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <div class="txt-video-r">
+                                    <div class="titulo-video ltt">
+                                        <h3>Cultura - Ciranda Luciana Ferreira </h3>
+                                    </div>  
+                                    <div class="desc ltt">
+                                        <p>Nesta nossa Quarta Cultural, vamos ver alguns registros da construção da Ciranda de Arte e Cultura  Luciana Ferreira, na ocupação Luisa Mahin em homenagem à vida e ao legado de nossa grande companheira!</p>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                        <ul class="videos">
+                            <li> 
+                                <a href="https://youtu.be/9eS6Q4ll4fY" class="vp-a">
+                                    <div class="youtube-video ltt" style="background-image:url(https://img.youtube.com/vi/9eS6Q4ll4fY/mqdefault.jpg)">
+                                        <img class="player-video" src="<?php echo get_template_directory_uri(); ?>/assets/images/player.png" alt="">
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <div class="txt-video-r">
+                                    <div class="titulo-video ltt">
+                                        <h3>Famílias do MTST receberam chaves de 910 apartamentos em Santo André</h3>
+                                    </div>  
+                                    <div class="desc ltt">
+                                        <p>Após 7 anos de luta, fruto da resistência dentro das ocupações do MTST, os condomínios Novo Pinheirinho e Santo Dias, foram inaugurados em março de 2019.</p>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
                     </div>
                 </div>
                 <div class="ver-tudo">
