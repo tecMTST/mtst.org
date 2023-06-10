961b831b5a782706c06ae675a79a86c0d622ac3a
dsilvadori <devmaquinads@gmail.com>
2023-06-10 13:30:54 -0300

    Implementa campos personalizados na área de podcasts

--- wp-content/themes/mtst-theme/template-parts/home/programas.php
+++ wp-content/themes/mtst-theme/template-parts/home/programas.php
@@ -2,22 +2,15 @@
             <div class="container">
                 <h2 class="titulo-secao">PROGRAMAS</h2>
                 <div class="row">
+                    <?php if( have_rows('programas') ): 
+                    while( have_rows('programas') ): the_row(); ?>
                     <div class="col-md-6">
                         <div class="embed-iframe">
-                        <iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/0NtLNSRpDL7XgbFioKbW7c?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-                        </div>
-                        <div class="embed-iframe">
-                            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/show/5fnmpe3C5puyTa1cCDZpIv?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+                            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/<?php the_sub_field('id_podcast'); ?>?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                         </div>
                     </div>
-                    <div class="col-md-6">
-                        <div class="embed-iframe">
-                            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/show/5X2nnU5w9uptyMiApbPCWj?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-                        </div>
-                        <div class="embed-iframe">
-                            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/563F90vbBEeqs14IJ8gLqo?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-                        </div>
-                    </div>
+                    <?php endwhile; 
+                    endif; ?>
                 </div>
                 <div class="ver-tudo"><a target="_blank" href="https://open.spotify.com/show/5X2nnU5w9uptyMiApbPCWj">VER TUDO >>></a></div>
             </div>
