368750dca7a6310920cfcb19e78243ffafcece9d
dsilvadori <devmaquinads@gmail.com>
2023-06-11 15:37:31 -0300

    Implementa custom fields para a área de fotos

--- wp-content/themes/mtst-theme/template-parts/home/fotos.php
+++ wp-content/themes/mtst-theme/template-parts/home/fotos.php
@@ -3,18 +3,18 @@
                     <h2 class="titulo-secao">FOTOS</h2>
                     <div class="row">
                         <div class="col-md-6">
-                            <div class="foto foto-maior">
+                            <div class="foto foto-maior desktop-view">
                                 <div class="swiper swiper-foto">
                                     <div class="swiper-wrapper">
+                                    <?php $cont = 0; 
+                                    if( have_rows('fotos') ): 
+                                    while( have_rows('fotos') && $cont < 3 ): the_row(); ?>
                                         <div class="swiper-slide">
-                                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/insta01.jpg" alt="Foto do Instagram">
+                                            <img src="<?php the_sub_field('imagem'); ?>" alt="Foto do Instagram">
                                         </div>
-                                        <div class="swiper-slide">
-                                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/insta02.jpg" alt="Foto do Instagram">
-                                        </div>
-                                        <div class="swiper-slide">
-                                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/insta03.jpg" alt="Foto do Instagram">
-                                        </div>
+                                    <?php $cont++;
+                                    endwhile; 
+                                    endif; ?>
                                     </div>
                                     <div class="swiper-pagination"></div>
                                     <div class="swiper-button-prev"></div>
@@ -23,37 +23,38 @@
                             </div>
                         </div>
                         <div class="col-md-3 desktop-view">
+                            <?php $cont = 0;
+                            if( have_rows('fotos') ): 
+                            while( have_rows('fotos') && $cont < 2 ): the_row(); ?>
                             <div class="foto foto-menor">
-                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/insta04.jpg" alt="Foto do Instagram">
+                                <img src="<?php the_sub_field('imagem'); ?>" alt="Foto do Instagram">
                             </div>
-                            <div class="foto foto-menor">
-                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/insta06.jpg" alt="Foto do Instagram">
-                            </div>
+                            <?php $cont++;
+                            endwhile; 
+                            endif; ?>
                         </div>
                         <div class="col-md-3 desktop-view">
+                            <?php $cont = 0;
+                            if( have_rows('fotos') ): 
+                            while( have_rows('fotos') && $cont < 2 ): the_row(); ?>
                             <div class="foto foto-menor">
-                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/insta05.jpg" alt="Foto do Instagram">
+                                <img src="<?php the_sub_field('imagem'); ?>" alt="Foto do Instagram">
                             </div>
-                            <div class="foto foto-menor">
-                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/insta07.jpg" alt="Foto do Instagram">
-                            </div>
+                            <?php $cont++;
+                            endwhile; 
+                            endif; ?>
                         </div>
                     </div>
                     <div class="fotos-mobile mobile-view">
                         <div class="swiper swiper-fotos-mob">
                             <div class="swiper-wrapper">
+                            <?php if( have_rows('fotos') ): 
+                            while( have_rows('fotos') ): the_row(); ?>
                                 <div class="swiper-slide">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/insta04.jpg" alt="Foto do Instagram">
+                                    <img src="<?php the_sub_field('imagem'); ?>" alt="Foto do Instagram">
                                 </div>
-                                <div class="swiper-slide">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/insta06.jpg" alt="Foto do Instagram">
-                                </div>
-                                <div class="swiper-slide">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/insta05.jpg" alt="Foto do Instagram">
-                                </div>
-                                <div class="swiper-slide">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/insta05.jpg" alt="Foto do Instagram">
-                                </div>
+                            <?php endwhile; 
+                            endif; ?>
                             </div>
                             <div class="swiper-pagination"></div>
                         </div>
